03358fb4fbfaa22be6173849ef2a544d835b6009
chilingirov <vasko988@gmail.com>
2022-03-29 17:18:33 -0700

    Removed build folder

--- .next/static/XLff18JJHCnqz6ggLoDTL/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/247-d44cf58cab562413.js","static/css/149b18973e5508c7.css","static/chunks/pages/index-042284a3bd12fb08.js"],"/_error":["static/chunks/pages/_error-1995526792b513b2.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
